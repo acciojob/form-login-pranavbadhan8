eb763710e23e4404ed5e69d9aacdf1354fedd5e6
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-05-25 13:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+    let fname=document.getElementsByTagName("input")[0].value;
+	let lname=document.getElementsByTagName("input")[1].value;
+	alert(fname+" "+lname)
 }
